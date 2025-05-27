b69655069a5e9912cb642981769ba50dd8afe037
Luca <152313871+Luca2040@users.noreply.github.com>
2025-05-27 15:41:20 +0200

    build url from parameters

--- url_jsonifier/test.py
+++ url_jsonifier/test.py
@@ -9,7 +9,7 @@ URL_DECODE_PATH = os.path.join(DATA_FOLDER, "decode.json")
 # Creates dir "data" if it doesnt exist
 os.makedirs(DATA_FOLDER, exist_ok=True)
 
-KIBANA_URL = "https://kibana-logging-devops-pcto.stella.cloud.az.cgm.ag/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:'2025-05-09T18:02:40.258Z',to:'2025-05-10T02:05:46.064Z'))&_a=(columns:!(agent.id,cometa.log.message,kubernetes.namespace,kubernetes.pod.name),dataSource:(dataViewId:'container-log*',type:dataView),filters:!(),grid:(columns:('@timestamp':(width:127),agent.id:(width:159),cometa.log.message:(width:249),kubernetes.namespace:(width:202))),interval:auto,query:(language:kuery,query:'kubernetes.pod.name%20:%20%22backend%22'),sort:!(!('@timestamp',desc)))"
+KIBANA_URL = "https://kibana-logging-devops-pcto.stella.cloud.az.cgm.ag/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:'2025-05-09T18:02:40.258Z',to:'2025-05-10T02:05:46.064Z'))&_a=(columns:!(agent.id,cometa.log.message,kubernetes.namespace,kubernetes.pod.name),dataSource:(dataViewId:'container-log*',type:dataView),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,field:kubernetes.namespace,index:'container-log*',key:kubernetes.namespace,negate:!f,params:(query:qa),type:phrase),query:(match_phrase:(kubernetes.namespace:qa)))),grid:(columns:('@timestamp':(width:127),agent.id:(width:159),cometa.log.message:(width:249),kubernetes.namespace:(width:202))),interval:auto,query:(language:kuery,query:'kubernetes.pod.name%20:%20%22backend%22'),sort:!(!('@timestamp',desc)))"
 
 COLOR_RED = "\033[91m"
 COLOR_RESET = "\033[0m"
