4975002f26ea25146b5437b6684f830e5e87b19f
shini161 <shini161os@gmail.com>
2025-05-28 09:38:31 +0200

    Added plugin test files

---
+++ container/cat/plugins/kibcat/plugin.py
@@ -0,0 +1,22 @@
+from cat.mad_hatter.decorators import tool, hook
+
+@hook
+def agent_prompt_prefix(prefix, cat):
+
+    prefix = """You always include the word "sun" in your replies
+"""
+
+    return prefix
+
+@tool(return_direct=True)
+def socks_prices(color, cat):
+    """How much do socks cost? Input is the sock color."""
+    prices = {
+        "black": 5,
+        "white": 10,
+        "pink": 50,
+    }
+    if color not in prices.keys():
+        return f"No {color} socks"
+    else:
+        return f"{prices[color]} €"
