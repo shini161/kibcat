46fd45bc933d1471cc065ab27a88264a59cda715
Luca <152313871+Luca2040@users.noreply.github.com>
2025-05-28 11:39:53 +0200

    added logger to load_template.py

--- json-template/load_template.py
+++ json-template/load_template.py
@@ -1,11 +1,7 @@
 from jinja2 import Template
 import os
-import sys
 import json
 
-# Do this to import from relative path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from url_jsonifier.utils import build_rison_url_from_json
 
 def render_dict(base_url,
                 start_time,
@@ -34,28 +30,3 @@ def render_dict(base_url,
     )
 
     return json.loads(output_str)
-
-
-# For testing
-if __name__ == "__main__":
-    base_url = "https://kibana-logging-devops-pcto.stella.cloud.az.cgm.ag/app/discover"
-    start_time = "2025-05-09T18:02:40.258Z"
-    end_time = "2025-05-10T02:05:46.064Z"
-    visible_fields = ["agent.id", "cometa.log.message", "kubernetes.namespace",
-                      "kubernetes.pod.name", "container.image.name", "cometa.log.level"]
-    filters = [("kubernetes.namespace", "qa"),
-               ("cometa.log.level", "ERROR")]
-    data_view_id = "container-log*"
-    search_query = "kubernetes.pod.name : \\\"backend\\\""
-
-    result_dict = render_dict(base_url,
-                              start_time,
-                              end_time,
-                              visible_fields,
-                              filters,
-                              data_view_id,
-                              search_query)
-
-    url = build_rison_url_from_json(json_dict=result_dict)
-
-    print(url)
