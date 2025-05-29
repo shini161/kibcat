c50ff751c5b7295d7a774426eb6fbcfc5cd789d0
shini161 <shini161os@gmail.com>
2025-05-29 16:16:51 +0200

    Finished Rewriting URL_JSONifier folder

--- src/url_jsonifier/tests/generate_test.py
+++ src/url_jsonifier/tests/generate_test.py
@@ -1,28 +1,28 @@
-from url_jsonifier.builders import build_rison_url_from_json
-from url_jsonifier.parsers import parse_rison_url_to_json
+from ..builders import build_rison_url_from_json
+from ..parsers import parse_rison_url_to_json
 import json
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_FOLDER = os.path.join(BASE_DIR, "data")
-URL_DECODE_PATH = os.path.join(DATA_FOLDER, "decode.json")
+OUTPUT_FILE_PATH = os.path.join(DATA_FOLDER, "parsed_rison.json")
 
 # Creates dir "data" if it doesnt exist
 os.makedirs(DATA_FOLDER, exist_ok=True)
 
 KIBANA_URL = (
-        "https://kibana-logging-devops-pcto.stella.cloud.az.cgm.ag/app/discover#/?_g="
+        "https://kibcat.com/app/discover#/?_g="
         "(filters:!(),refreshInterval:(pause:!t,value:60000),"
         "time:(from:'2025-05-09T18:02:40.258Z',to:'2025-05-10T02:05:46.064Z'))&_a="
-        "(columns:!(agent.id,cometa.log.message,kubernetes.namespace,kubernetes.pod.name),"
-        "dataSource:(dataViewId:'container-log*',type:dataView),"
+        "(columns:!(choco.id,log.message,example.namespace,example.name),"
+        "dataSource:(dataViewId:'logs*',type:dataView),"
         "filters:!(('$state':(store:appState),"
-        "meta:(alias:!n,disabled:!f,field:kubernetes.namespace,index:'container-log*',"
-        "key:kubernetes.namespace,negate:!f,params:(query:qa),type:phrase),"
-        "query:(match_phrase:(kubernetes.namespace:qa)))),"
-        "grid:(columns:('@timestamp':(width:127),agent.id:(width:159),"
-        "cometa.log.message:(width:249),kubernetes.namespace:(width:202))),"
-        "interval:auto,query:(language:kuery,query:'kubernetes.pod.name%20:%20%22backend%22'),"
+        "meta:(alias:!n,disabled:!f,field:example.namespace,index:'logs*',"
+        "key:example.namespace,negate:!f,params:(query:qa),type:phrase),"
+        "query:(match_phrase:(example.namespace:qa)))),"
+        "grid:(columns:('@timestamp':(width:127),choco.id:(width:159),"
+        "log.message:(width:249),example.namespace:(width:202))),"
+        "interval:auto,query:(language:kuery,query:'example.name%20:%20%22backend%22'),"
         "sort:!(!('@timestamp',desc)))"
         )
 
@@ -31,11 +31,16 @@ COLOR_RESET = "\033[0m"
 
 # Parse Kibana URL to JSON
 print(f"{COLOR_RED}---- PARSE URL ----{COLOR_RESET}")
-url_to_json = parse_rison_url_to_json(KIBANA_URL, URL_DECODE_PATH)
-print(json.dumps(url_to_json, indent=2))
+json_url = parse_rison_url_to_json(KIBANA_URL, OUTPUT_FILE_PATH)
+print(json.dumps(json_url, indent=2))
 
 
-# Rebuild Kibana URL from JSON
-print(f"{COLOR_RED}---- BUILD URL ----{COLOR_RESET}")
-json_to_url = build_rison_url_from_json(URL_DECODE_PATH)
-print(f"{json_to_url}")
+# Rebuild Kibana URL from JSON from file
+print(f"{COLOR_RED}---- BUILD URL FROM FILE ----{COLOR_RESET}")
+rison_url_from_file = build_rison_url_from_json(OUTPUT_FILE_PATH)
+print(f"{rison_url_from_file}")
+
+# Rebuild Kibana URL from JSON from json
+print(f"{COLOR_RED}---- BUILD URL FROM JSON ----{COLOR_RESET}")
+rison_url_from_json = build_rison_url_from_json(None, json_url)
+print("f{rison_url_from_json}")
