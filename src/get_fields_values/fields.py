f14df33215e5fcc4b04c8e6b44d241bcaa9d491d
Luca <152313871+Luca2040@users.noreply.github.com>
2025-06-04 10:54:58 +0200

    get the values list

--- src/get_fields_values/fields.py
+++ src/get_fields_values/fields.py
@@ -4,6 +4,10 @@ from elasticsearch import Elasticsearch
 from dotenv import load_dotenv
 import re
 import os
+from elastic_transport import NodeConfig
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 load_dotenv()
 
@@ -18,28 +22,55 @@ if not BASE_URL or not USERNAME or not PASS:
 
     print(msg)
     exit(1)
-    
+
 
 print(f"Connecting to elastic at: {BASE_URL}")
 
 
-es = Elasticsearch(BASE_URL, http_auth=(USERNAME, PASS), verify_certs=False)
-
-# Query elastic
-response = es.search(
-    index=DATA_VIEW_ID,
-    size=0,
-    aggs={"pod_names": {"terms": {"field": "kubernetes.pod.name", "size": 10000}}},
+node_config = NodeConfig(
+    scheme="https",
+    host=BASE_URL.split("://")[-1].split(":")[0],
+    port=443,
+    verify_certs=False,
 )
+
+es = Elasticsearch([node_config], basic_auth=(USERNAME, PASS))
+
+print(es.info())
 
 
 def extract_base_name(pod_name):
-    match = re.match(r"^([a-zA-Z0-9\-]+?)(?:-[0-9a-z]+.*)?$", pod_name)
+    match = re.match(r"^([a-zA-Z0-9\-]+?)(?:-[a-z0-9]+.*)?$", pod_name)
     return match.group(1) if match else pod_name
 
 
-pod_names = [bucket["key"] for bucket in response["aggregations"]["pod_names"]["buckets"]]
-base_names = sorted(set(extract_base_name(name) for name in pod_names))
+all_base_names = set()
+after_key = None
 
-# Output
-print(base_names)
+while True:
+    body = {
+        "size": 0,
+        "aggs": {
+            "pod_names": {
+                "composite": {
+                    "size": 1000,
+                    "sources": [{"pod_name": {"terms": {"field": "kubernetes.pod.name.keyword"}}}],
+                    **({"after": after_key} if after_key else {}),
+                }
+            }
+        },
+    }
+
+    response = es.search(index=".ds-container-log-monthly-*", body=body)
+    buckets = response["aggregations"]["pod_names"]["buckets"]
+
+    for bucket in buckets:
+        pod_name = bucket["key"]["pod_name"]
+        base_name = extract_base_name(pod_name)
+        all_base_names.add(base_name)
+
+    after_key = response["aggregations"]["pod_names"].get("after_key")
+    if not after_key:
+        break
+
+print(sorted(all_base_names))
