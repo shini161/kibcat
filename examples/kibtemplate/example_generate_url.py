a8a6ffa0db127735c45ba9f0f02959d0b1a49619
Luca <152313871+Luca2040@users.noreply.github.com>
2025-06-05 14:22:30 +0200

    removed unnecessary import

--- examples/kibtemplate/example_generate_url.py
+++ examples/kibtemplate/example_generate_url.py
@@ -4,8 +4,6 @@ from kiblog import BaseLogger
 from kibtemplate import build_template, KibCatFilter, FilterOperators
 from kiburl import build_rison_url_from_json
 
-import os
-
 
 # pylint: disable=too-many-positional-arguments
 def generate_kibana_url(
