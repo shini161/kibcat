b4b3dc15df505036aaa0138e2cc6e2764489a356
Matteo <matteo.gheza07@gmail.com>
2025-06-06 16:50:40 +0200

    Remove debug form message

--- cat/plugins/kibcat/plugin.py
+++ cat/plugins/kibcat/plugin.py
@@ -279,30 +279,12 @@ class FilterForm(CatForm):
 
     def _generate_base_message(self) -> str:
         """Generates the base message for form incomplete response."""
-        dump_obj = deepcopy(self._model)
-        dump_obj["filters"] = [
-            filter_element.model_dump() for filter_element in dump_obj["filters"]
-        ]
-
         separator = "\n - "
-        missing_fields = ""
-        if self._missing_fields:
-            missing_fields = "\nMissing fields:"
-            missing_fields += separator + separator.join(self._missing_fields)
         invalid_fields = ""
         if self._errors:
-            invalid_fields = "\nInvalid fields:"
+            invalid_fields = "\nDati non corretti:"
             invalid_fields += separator + separator.join(self._errors)
-
-        out = f"""Info until now:
-
-```json
-{json.dumps(dump_obj, indent=4)}
-```
-{missing_fields}
-{invalid_fields}
-"""
-        return out
+        return invalid_fields
 
     def validate(self):
         """Validate form data"""
