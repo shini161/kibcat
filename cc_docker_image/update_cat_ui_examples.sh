7cfadaa143c8b8a84270e1d28d3a21f30610af07
Matteo <matteo.gheza07@gmail.com>
2025-06-10 10:44:27 +0200

    Add CSS style override to Cat UI
    Co-authored-by: shini161 <85759040+shini161@users.noreply.github.com>

--- cc_docker_image/update_cat_ui_examples.sh
+++ cc_docker_image/update_cat_ui_examples.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 js_file="/admin/assets/cat.js"
+css_file="/admin/assets/cat.css"
 
 absolute_path=$(realpath "$0")
 script_dir=$(dirname "$absolute_path")
@@ -28,3 +29,23 @@ newArray=$(jq -c '.' "$example_messages_file")
 sed -i "s|defaultMessages:\[[^]]*\]|defaultMessages:$newArray|" "$js_file"
 
 echo "Replaced defaultMessages array in $js_file"
+
+### UPDATE CSS ###
+
+# Check if source CSS file exists
+if [ ! -f "$css_file" ]; then
+    echo "Error: cat.css not found" >&2
+    exit 1
+fi
+
+style_override_file="${script_dir}/style_override.css"
+
+# Check if the file exists
+if [ ! -f "$style_override_file" ]; then
+    echo "Error: style_override.css not found" >&2
+    exit 1
+fi
+
+# Append the content of style_override.css to cat.css
+cat "$style_override_file" >> "$css_file"
+echo "Appended style_override.css to $css_file"
