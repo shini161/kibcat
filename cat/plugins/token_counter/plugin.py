bd4a6ff826ba57810ea68b4db6571cb20d8854eb
Matteo <matteo.gheza07@gmail.com>
2025-06-11 12:43:08 +0200

    Add TokenCounter plugin

---
+++ cat/plugins/token_counter/plugin.py
@@ -0,0 +1,46 @@
+from typing import Any
+
+import tiktoken
+from cat.mad_hatter.decorators import hook, tool
+from langchain_core.callbacks.base import BaseCallbackHandler
+from langchain_core.outputs import LLMResult
+
+
+class TokenCounterHandler(BaseCallbackHandler):
+    def __init__(self, model_name: str = "gpt-4o-mini"):
+        self.input_tokens = 0
+        self.output_tokens = 0
+        self.encoding = tiktoken.encoding_for_model(model_name)
+
+    def _count_tokens(self, text: str) -> int:
+        """Count the number of tokens in a given text."""
+        return len(self.encoding.encode(text))
+
+    def on_llm_start(self, serialized: dict[str, Any], prompts: list[str], **kwargs: Any) -> None:
+        # Get corresponding encoding for the model
+        self.input_tokens += self._count_tokens(prompts[0])
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        # Get corresponding encoding for the model
+        self.output_tokens += self._count_tokens(response.generations[0][0].text)
+
+
+@hook
+def after_cat_bootstrap(cat):
+    callback_token_counter = TokenCounterHandler(model_name=cat._llm.model_name)
+    cat._llm.callbacks = [callback_token_counter]
+    print("Token counting handler initialized.")
+
+
+@tool(return_direct=True)
+def get_token_count(tool_input, cat):
+    """
+    Get the total number of output tokens used by the LLM. Input is always None.
+    """
+    try:
+        return (
+            f"Input tokens: {cat._llm.callbacks[0].input_tokens}\n"
+            f"Output tokens: {cat._llm.callbacks[0].output_tokens}"
+        )
+    except Exception as e:
+        return f"Error retrieving token count: {e}"
