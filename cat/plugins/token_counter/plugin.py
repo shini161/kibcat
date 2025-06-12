7b92cf748eefa51adba2412c891cc71e3cd3331f
Luca <152313871+Luca2040@users.noreply.github.com>
2025-06-12 16:35:51 +0200

    token counter fix

--- cat/plugins/token_counter/plugin.py
+++ cat/plugins/token_counter/plugin.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import tiktoken
+from cat.convo.messages import CatMessage
 from cat.log import log
 from cat.mad_hatter.decorators import hook, tool
 from langchain_core.callbacks.base import BaseCallbackHandler
@@ -17,7 +18,9 @@ class TokenCounterHandler(BaseCallbackHandler):
         """Count the number of tokens in a given text."""
         return len(self.encoding.encode(text))
 
-    def on_llm_start(self, serialized: dict[str, Any], prompts: list[str], **kwargs: Any) -> None:
+    def on_llm_start(
+        self, serialized: dict[str, Any], prompts: list[str], **kwargs: Any
+    ) -> None:
         # Get corresponding encoding for the model
         self.input_tokens += self._count_tokens(prompts[0])
 
@@ -31,6 +34,18 @@ def after_cat_bootstrap(cat):
     callback_token_counter = TokenCounterHandler(model_name=cat._llm.model_name)
     cat._llm.callbacks = [callback_token_counter]
     log.info("Token counting handler initialized.")
+
+
+@hook
+def agent_fast_reply(fast_reply, cat):
+
+    history = cat.working_memory.history
+    if not any([isinstance(element, CatMessage) for element in history]):
+        # If the user has reset the conversation then reset the token count
+        cat._llm.callbacks[0].input_tokens = 0
+        cat._llm.callbacks[0].output_tokens = 0
+
+    return fast_reply
 
 
 @tool(return_direct=True)
