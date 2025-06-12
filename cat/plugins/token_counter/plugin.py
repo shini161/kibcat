2b6e234f0ea31d1484dbee52786f6248605aa014
Matteo <matteo.gheza07@gmail.com>
2025-06-12 17:19:07 +0200

    Fix mypy errors

--- cat/plugins/token_counter/plugin.py
+++ cat/plugins/token_counter/plugin.py
@@ -4,8 +4,8 @@ import tiktoken
 from cat.convo.messages import CatMessage
 from cat.log import log
 from cat.mad_hatter.decorators import hook, tool
-from langchain_core.callbacks.base import BaseCallbackHandler  # type: ignore
-from langchain_core.outputs import LLMResult
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import LLMResult
 
 
 class TokenCounterHandler(BaseCallbackHandler):
